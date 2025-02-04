619e6cb5fadf2f9ee8f497a187409226b65bc4de
Balaena Institute whitehead <anaeguibur@gmail.com>
2025-02-04 16:29:08 -0500

    added gimbal angle from FlightReader logs

--- Scripts/Gal23_DroneMeasurementError_DataPrep.R
+++ Scripts/Gal23_DroneMeasurementError_DataPrep.R
@@ -7,6 +7,7 @@ library("plotly")
 library(ggplot2)
 library(lubridate)
 
+
 #a <- getMorphoMetrix(ROOTfolderpath = "/Users/anacristinaeguigurenburneo/Documents/Galapagos2023_Drone_Snapshots/Balaena_Callibrations/")
 #write.csv(a, "/Users/anacristinaeguigurenburneo/Documents/Galapagos_Fieldwork_2020s/Measurement_Error_Outputs/Balaena_Raw_Measures.csv")
 
@@ -34,7 +35,6 @@ f$videoFileBase <- basename(f$videoFile)
 
 f$Date <- gsub(" .*", "", f$LocalTime)
 
-library(lubridate)
 f$Month <- as.character(month(ymd(010101) + months(as.numeric(substr(f$Date, 7,7))-1),label=TRUE,abbr=TRUE))
 f$DateWord <- paste(substr(f$Date, 9,10), f$Month, "23", sep = "-")
 
@@ -54,9 +54,79 @@ dat$FlightNo <- factor(dat$FlightNo)
 
 # 2. clean up
 dat <- dat %>% select(
-  imageName, timeStamp, altitude, length ,pixelDimension.old = pixelDimension, ImageWidth ,
+  imageName, timeStamp, LocalTime ,altitude, length ,pixelDimension.old = pixelDimension, ImageWidth ,
   balaenaAngled, pos, FlightNo, Date = Date.y
 )
 
-rm(a);rm(f);rm(m);rm(s)
+# 3. assign gimbal angle -----
+#~~~~a. read in flight logs----
+#Define the folder path where flight logs are stored
+#FLIGHTREADfolderpath <- "D:/Gal2023_Drone/FlightReader_logs/"
+
+# Read all CSV files and merge into a single dataframe
+#tmp.files <- list.files(path = FLIGHTREADfolderpath, pattern = "*.csv", full.names = TRUE, recursive = T)
+#df_list <- lapply(tmp.files, read.csv)
+#df_list <- lapply(df_list, function(df) {
+#  df %>% mutate(across(everything(), as.character))
+#})
+
+
+
+#d <- bind_rows(df_list)
+
+# Convert datetime to proper format
+#d$datetime_utc6 <- mdy_hms(d$CUSTOM.updateDateTime24, tz = "Etc/GMT+6")
+
+# Arrange data by timestamp
+#d <- d %>%
+#  arrange(datetime_utc6, OSD.flyTime..s.) %>%
+#  select(OSD.flyTime..s., datetime_utc6, OSD.height..m., GIMBAL.pitch)
+
+#write.csv(d, "Raw_Data/FlightReader_Flight_logs.csv")
+
+d<- read.csv("Raw_Data/FlightReader_Flight_logs.csv", header = T)
+
+d <- d %>%
+  distinct(datetime_utc6, .keep_all = TRUE)
+
+d$GIMBAL.pitch <- as.numeric(d$GIMBAL.pitch)
+
+d$datetime_utc6 <- ymd_hms(d$datetime_utc6, tz = "Etc/GMT+6")
+
+
+#~~~~b.format snapshot date times----
+dat$videoLocalTime <- ymd_hms(dat$LocalTime, tz ="Etc/GMT+6") #
+
+# add snapshot time stamp:
+
+
+dat <- dat %>% mutate(
+  timeParts = strsplit(timeStamp, ":"), 
+  minutes = as.numeric(sapply(timeParts, `[`, 1)),
+  seconds = as.numeric(sapply(timeParts, `[`, 2)),
+  # Convert to total seconds and add to datetime
+  datetime_utc6 = videoLocalTime + minutes*60 +seconds
+)
+
+
+
+#~~~~c. join based on datetime ------
+dat <- left_join(dat, d, by = "datetime_utc6")
+
+#check altitude errors
+
+dat$altitude_error <- dat$altitude-as.numeric(dat$OSD.height..m.)
+
+hist(dat$altitude_error, breaks = 20)
+
+x<-dat[sample(which(abs(dat$altitude_error)>10), 10, replace = F),]
+
+x<-dat[which(abs(dat$altitude_error)>10),] # omit the weird ones 
+
+
+
+
+head(dat)
+
+rm(a);rm(f);rm(m);rm(s); rm(x); rm(d); rm(df_list)
 
